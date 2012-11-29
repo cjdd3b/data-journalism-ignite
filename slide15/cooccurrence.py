046ca72a3f1f007434e48ee3d57fe2231427a157
Chase Davis <chase.davis@gmail.com>
2012-11-29 15:13:52 -0800

    Slide 15 documentation

--- slide15/cooccurrence.py
+++ slide15/cooccurrence.py
@@ -3,14 +3,18 @@ import re
 import operator
 import math
 
-dj_text = open('datajournalism.txt', 'r').read().lower()
-dj_words = re.findall(r'\w+', dj_text, flags = re.UNICODE | re.LOCALE) 
-dj_important_words = filter(lambda x: x not in nltk.corpus.stopwords.words('english'), dj_words)
+# Uses basically the same cleaning techniques as found in Slide 5. Removes stopwords like "and", "the", etc.,
+# ignores punctuation and changes all words to lowercase
+dj_text = open('datajournalism.txt', 'r').read().lower() # Open the file and convert to lowercase
+dj_words = re.findall(r'\w+', dj_text, flags = re.UNICODE | re.LOCALE) # Ignore punctuation
+dj_important_words = filter(lambda x: x not in nltk.corpus.stopwords.words('english'), dj_words) # Remove stopwords
 
 ds_text = open('datascience.txt', 'r').read().lower()
 ds_words = re.findall(r'\w+', ds_text, flags = re.UNICODE | re.LOCALE) 
 ds_important_words = filter(lambda x: x not in nltk.corpus.stopwords.words('english'), ds_words)
 
+# Now we're going to try to find words that occur frequently, and in relatively equal proportions,
+# in both documents
 bothdocs = {}
 for word in dj_important_words:
     if word in ds_important_words:
@@ -22,8 +26,9 @@ for word in dj_important_words:
             / (abs((dj_important_words.count(word) - ds_important_words.count(word))) + 1.0) \
                 * math.log((dj_important_words.count(word) + ds_important_words.count(word)))
 
-terms = sorted(bothdocs.iteritems(), key=operator.itemgetter(1))
-terms.reverse()
+# Sort the list from the highest score to the lowest
+terms = sorted(bothdocs.iteritems(), key=operator.itemgetter(1), reverse=True)
 
+# Print the top 25 terms
 for term in terms[:25]:
     print term
